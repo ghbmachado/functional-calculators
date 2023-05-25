4e880a6254df56fb9beec7f869e51d5a734eea72
Gabriel Machado <97855834+ghbmachado@users.noreply.github.com>
2023-05-25 16:48:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,108 @@
+function ex1() {
+    let n1 = Number(document.getElementById("n1").value)
+    let n2 = Number(document.getElementById("n2").value)
+  
+    let result = n1 - n2
+  
+    alert("O resultado é: " + result)
+  }
+  
+  function ex2() {
+    let nu1 = Number(document.getElementById("nu1").value)
+    let nu2 = Number(document.getElementById("nu2").value)
+    let nu3 = Number(document.getElementById("nu3").value)
+  
+    let result2 = nu1 * nu2 * nu3
+  
+    alert("A multiplicação dos número resulta-se em: " + result2)
+  }
+  
+  function ex3() {
+    let num1 = Number(document.getElementById("num1").value)
+    let num2 = Number(document.getElementById("num2").value)
+  
+    if (num2 == 0) {
+      alert("Divisor inválido!")
+    } else {
+      let result3 = num1 / num2
+  
+      alert("A divisão resulta-se em: " + result3)
+    }
+  }
+  
+  function ex4() {
+    let numb1 = Number(document.getElementById("numb1").value)
+    let numb2 = Number(document.getElementById("numb2").value)
+  
+    let result4 = (numb1 * 2 + numb2 * 3) / 5
+  
+    alert("A média ponderada das notas é: " + result4)
+  }
+  
+  function ex5() {
+    let numbe1 = Number(document.getElementById("numbe1").value)
+  
+    let desconto = numbe1 * 0.1
+    let result5 = numbe1 - desconto
+  
+    alert("O novo preço é R$" + result5)
+  }
+  
+  function ex6() {
+    let number1 = Number(document.getElementById("number1").value)
+    let number2 = Number(document.getElementById("number2").value)
+  
+    let comissao = number2 * 0.04
+    let result6 = number1 + comissao
+  
+    alert(
+      "A comissão do funcionário é R$ " +
+        comissao +
+        " ,logo seu salário final é R$ " +
+        result6
+    )
+  }
+  
+  function ex7() {
+    let weight = Number(document.getElementById("weight").value)
+  
+    let more = weight * 0.15
+    let less = weight * 0.2
+  
+    let resultmore7 = weight + more
+    let resultless7 = weight - less
+  
+    alert(
+      "Seu peso caso engorde 15% é " +
+        resultmore7 +
+        " kg, e seu peso caso emagreça 20% é " +
+        resultless7 +
+        " kg."
+    )
+  }
+  
+  function ex8() {
+    let weight1 = Number(document.getElementById("weight1").value)
+  
+    let result8 = weight1 * 1000
+  
+    alert("O seu peso em gramas é: " + result8 + " g.")
+  }
+  
+  function ex9() {
+    let bb = Number(document.getElementById("bb").value)
+    let mb = Number(document.getElementById("mb").value)
+    let height = Number(document.getElementById("height").value)
+  
+    let result9 = ((bb + mb) * height) / 2
+  
+    alert("A área do trapézio é: " + result9)
+  }
+  
+  function ex10() {
+    let side = Number(document.getElementById("side").value)
+  
+    let result10 = side * side
+  
+    alert("A área do quadrado é: " + result10)
+  }
